609903a8c2ca9dd0fe05a40feacaffefa8392884
Yustynn <yustynn@gmail.com>
2015-10-14 23:47:29 +0800

    Add basic game functionality, load jQuery through CDN

---
+++ js/app.js
@@ -0,0 +1,72 @@
+// I tried out the Mammals spec, and figured this would be good practice.
+
+function Game() {
+  // randomly generate answer
+  this.answer = Math.ceil(Math.random() * 100);
+  // initialize guesses left to 5
+  this.numGuessesLeft = 5;
+
+  // store references to nodes (for speed)
+  this.guessElement = $('#guessbox');
+  this.numGuessesLeftNode = $('#num-guesses');
+
+  // reset displayed guesses left to 5
+  this.updateNumGuessesDisplay();
+}
+
+// return value of guess
+Game.prototype.retrieveGuess = function() {
+  console.log(this.guessElement.val());
+  return this.guessElement.val();
+};
+
+// return true/false for if guess is correct
+Game.prototype.correctGuess = function() {
+  return this.retrieveGuess() == this.answer;
+};
+
+// update only displayed guesses left
+Game.prototype.updateNumGuessesDisplay = function() {
+  this.numGuessesLeftNode.text(this.numGuessesLeft);
+};
+
+// update guesses left. Return lose() if no more guesses left.
+Game.prototype.minusGuess = function() {
+  this.numGuessesLeft--;
+  this.updateNumGuessesDisplay();
+  if (this.numGuessesLeft == 0)
+    return this.lose();
+};
+
+// return win() if correct guess. Return minusGuess() if wrong guess.
+Game.prototype.evaluateAnswer = function() {
+  if (this.correctGuess())
+    return this.win();
+  else
+    this.minusGuess();
+};
+
+// tell the player the good news
+Game.prototype.win = function() {
+  alert('YOU WIN!');
+};
+
+// tell the player to suck it
+Game.prototype.lose = function() {
+  alert('YOU LOSE! The right answer was ' + this.answer);
+};
+
+Game.prototype.turn = function(e) {
+  e.preventDefault();
+  debugger;
+  this.evaluateAnswer();
+}
+
+$(document).ready(function() {
+  var game = new Game;
+
+  $('.submit-guess').on('click', function(e) {
+    e.preventDefault();
+    game.evaluateAnswer();
+  });
+});
