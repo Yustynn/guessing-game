897cbbb9a3b43987a2e46e5000fdbb6466cd5b7d
Yustynn <yustynn@gmail.com>
2015-10-15 00:14:58 +0800

    Refactor, add enter button functionality

--- js/app.js
+++ js/app.js
@@ -1,67 +1,85 @@
-// I tried out the Mammals spec, and figured this would be good practice.
-
-function Game() {
-  // randomly generate answer
-  this.answer = Math.ceil(Math.random() * 100);
-  // initialize guesses left to 5
-  this.numGuessesLeft = 5;
-
-  // store references to nodes (for speed)
-  this.guessElement = $('#guessbox');
-  this.numGuessesLeftNode = $('#num-guesses');
-
-  // reset displayed guesses left to 5
-  this.updateNumGuessesDisplay();
-}
-
-// return value of guess
-Game.prototype.retrieveGuess = function() {
-  console.log(this.guessElement.val());
-  return this.guessElement.val();
-};
-
-// return true/false for if guess is correct
-Game.prototype.correctGuess = function() {
-  return this.retrieveGuess() == this.answer;
-};
-
-// update only displayed guesses left
-Game.prototype.updateNumGuessesDisplay = function() {
-  this.numGuessesLeftNode.text(this.numGuessesLeft);
-};
-
-// update guesses left. Return lose() if no more guesses left.
-Game.prototype.minusGuess = function() {
-  this.numGuessesLeft--;
-  this.updateNumGuessesDisplay();
-  if (this.numGuessesLeft == 0)
-    return this.lose();
-};
-
-// return win() if correct guess. Return minusGuess() if wrong guess.
-Game.prototype.turn = function() {
-  if (this.correctGuess())
-    return this.win();
-  else
-    this.minusGuess();
-};
-
-// tell the player the good news
-Game.prototype.win = function() {
-  alert('YOU WIN!');
-};
-
-// tell the player to suck it
-Game.prototype.lose = function() {
-  alert('YOU LOSE! The right answer was ' + this.answer);
-};
+// I tried out the Mammals spec, and figured making the game
+// this way would be good practice.
 
 $(document).ready(function() {
+  // store references to nodes (for speed)
+  var guessElement = $('#guessbox');
+  var numGuessesLeftNode = $('#num-guesses');
+
+  function Game() {
+    // randomly generate answer
+    this.answer = Math.ceil(Math.random() * 100);
+    // initialize guesses left to 5
+    this.numGuessesLeft = 5;
+
+    // reset displayed guesses left to 5
+    this.updateNumGuessesDisplay();
+  }
+
+  // return value of guess
+  Game.prototype.retrieveGuess = function() {
+    console.log(guessElement.val());
+    return guessElement.val();
+  };
+
+  // return true/false for if guess is correct
+  Game.prototype.correctGuess = function() {
+    return this.retrieveGuess() == this.answer;
+  };
+
+  // update only displayed guesses left
+  Game.prototype.updateNumGuessesDisplay = function() {
+    numGuessesLeftNode.text(this.numGuessesLeft);
+  };
+
+  // update guesses left. Return lose() if no more guesses left.
+  Game.prototype.minusGuess = function() {
+    this.numGuessesLeft--;
+    this.updateNumGuessesDisplay();
+    if (this.numGuessesLeft == 0)
+      return this.lose();
+  };
+
+  // return win() if correct guess. Return minusGuess() if wrong guess.
+  Game.prototype.turn = function() {
+    if (this.correctGuess())
+      return this.win();
+    else
+      this.minusGuess();
+  };
+
+  // tell the player the good news
+  Game.prototype.win = function() {
+    alert('YOU WIN!');
+  };
+
+  // tell the player to suck it
+  Game.prototype.lose = function() {
+    alert('YOU LOSE! The right answer was ' + this.answer);
+  };
+
+  // tell the player the answer
+  Game.prototype.giveHint = function() {
+    alert("Here's a hint: the answer's " + this.answer);
+  }
+
+
+
+
   var game = new Game;
+
+
 
   $('#submit-guess').on('click', function(e) {
     e.preventDefault(); // Just in case
     game.turn();
+  });
+
+
+  $('#guessbox').on('keyup', function(e) {
+    e.preventDefault(); // Just in case
+    if (e.keyCode == 13)
+      game.turn();
   });
 
   $('#restart').on('click', function(e) {
